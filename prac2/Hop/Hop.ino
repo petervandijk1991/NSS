27f36cdb7d4d427f1f65396ec1b9e5cf625d9ec0
Liz <e.a.schermerhorn93@gmail.com>
2014-05-09 14:56:30 +0200

    het verslag

--- prac2/Hop/Hop.ino
+++ prac2/Hop/Hop.ino
@@ -29,10 +29,6 @@
 
 RF24 radio(3, 9);
 
-// sets the role of this unit in hardware.  Connect to GND to be the 'pong' receiver
-// Leave open to be the 'ping' transmitter
-const int role_pin = 7;
-
 //
 // Topology
 //
@@ -40,55 +36,15 @@ const int role_pin = 7;
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[4] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL, 0xF0F0F0F0C3LL, 0xF0F0F0F0B4LL};
 
-//
-// Role management
-//
-// Set up role.  This sketch uses the same software for all the nodes
-// in this system.  Doing so greatly simplifies testing.  The hardware itself specifies
-// which node it is.
-//
-// This is done through the role_pin
-//
-
-// The various roles supported by this sketch
-typedef enum { role_ping_out = 1, role_pong_back } role_e;
 
 // The debug-friendly names of those roles
 const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back"};
 
-
-// The role of the current running sketch
-role_e role;
-
 void setup(void)
 {
-  //
-  // Role
-  //
+   Serial.begin(57600);
+  
 
-  // set up the role pin
-  pinMode(role_pin, INPUT);
-  digitalWrite(role_pin,HIGH);
-  delay(20); // Just to get a solid reading on the role pin
-
-  // read the address pin, establish our role
-  if ( ! digitalRead(role_pin) )
-    role = role_ping_out;
-  else
-    role = role_pong_back;
-
-  //
-  // Print preamble
-  //
-
-  Serial.begin(57600);
-  printf_begin();
-  printf("\n\rRF24/examples/pingpair/\n\r");
-  printf("ROLE: %s\n\r",role_friendly_name[role]);
-
-  //
-  // Setup and configure rf radio
-  //
 
   radio.begin();
 
@@ -113,7 +69,7 @@ void setup(void)
     radio.openReadingPipe(2,pipes[0]);
     
     //radio.openWritingPipe(pipes[2]);
-    //radio.openReadingPipe(1,pipes[3]);
+    radio.openReadingPipe(1,pipes[3]);
   
    // radio.openWritingPipe(pipes[1]);
     
@@ -146,13 +102,16 @@ void loop(void)
  
     // if there is data ready
     printf("Is de radio vrij?");
+    printf("\b", radio.available());
     if ( radio.available() )
     {
+      printf("ik zit in het if-statement");
       // Dump the payloads until we've gotten everything
       unsigned long message;
       bool done = false;
       while (!done)
       {
+        printf("Fetch payload!!! radio.read");
         // Fetch the payload, and see if this was the last one.
         done = radio.read( &message, sizeof(unsigned long) );
 
